21c7130100ad32aa69d3d3437202958294fac9dc
adhityan <adhityan@hotmail.com>
2019-05-01 17:24:31 +0400

    updated lint-staged and eslint config to support react

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     linters: {
-        '**/*.+(js|ts)': [
+        '**/*.+(js|ts|tsx|jsx)': [
             'eslint --fix',
             'jest --findRelatedTests',
             'git add'
         ],
-        '**/*.+(js|md|ts|css|sass|yml|yaml|scss|json)': [
+        '**/*.+(js|md|ts|css|sass|yml|yaml|scss|json|tsx|jsx)': [
             'prettier --write',
             'git add'
         ]
