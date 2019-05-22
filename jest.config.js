d6edd121ffa51b895c1c21c372b1556400207a0f
adhityan <adhityan@hotmail.com>
2019-05-22 13:13:12 +0400

    jest config formatted

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    rootDir: '.',
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['<rootDir>/tests/**/*.spec.ts'],
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.ts',
-      '!<rootDir>/src/**/*.d.ts',
-      '!<rootDir>/src/**/*.spec.ts',
-      '!<rootDir>/src/**/__*__/*',
-    ],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json']
-  }
+  rootDir: '.',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [ '<rootDir>/tests/**/*.spec.ts' ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/**/*.spec.ts',
+    '!<rootDir>/src/**/__*__/*',
+  ],
+  moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json' ]
+}
