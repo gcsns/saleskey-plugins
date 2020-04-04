41b08b0e6a289749d0b8eee2c3d047d0e3b16f21
Adhityan <adhityan@hotmail.com>
2020-04-04 22:18:07 +0400

    feat: removed git add as new husky does this automatically

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,11 +2,9 @@ module.exports = {
     '*.{js,ts,tsx,jsx}': [
         'eslint --fix',
         'prettier --write',
-        //'jest --findRelatedTests',
-        'git add'
+        //'jest --findRelatedTests'
     ],
     '*.{md,css,sass,yml,yaml,scss,json,html}': [
-        'prettier --write',
-        'git add'
+        'prettier --write'
     ]
 }
